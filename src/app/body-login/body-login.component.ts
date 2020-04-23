716c9c844aaefd401710a3a880355be08f7a2301
Yanlian Xin <xinyanlian@outlook.com>
2020-04-23 10:27:31 -0400

    ViewChild pass property from child to parent

--- src/app/body-login/body-login.component.ts
+++ src/app/body-login/body-login.component.ts
@@ -6,7 +6,7 @@ import {Component,Input} from '@angular/core';
 })
 
 export class BodyLoginComponent{
-    userName:string="addsls";
+    userName:string;
     isLogin:boolean=false;
     @Input() login: Function; 
     
--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,15 +1,25 @@
-import {Component} from '@angular/core';
+import {Component,ViewChild} from '@angular/core';
+import {BodyLoginComponent} from '../body-login/body-login.component';
 
 @Component({
   selector:'app-body',
   templateUrl:'./body.component.html'
 })
 
+
 export class BodyComponent{
-  username:string = 'YL';
+  @ViewChild(BodyLoginComponent)bodyLoginComponent:BodyLoginComponent;
+  username:string;
+  ngAfterViewInit(){
+    // console.log(this.bodyLoginComponent);
+    this.username=this.bodyLoginComponent.userName;
+  }
+
+  
   isLogin:boolean=false;
   login(){
     this.isLogin =!this.isLogin;
+    this.username=this.bodyLoginComponent.userName;
   }
  
 }
