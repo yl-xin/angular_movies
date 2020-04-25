c59293a2061bab4381a2f10ab84d48a8bc94acc1
Yanlian Xin <xinyanlian@outlook.com>
2020-04-25 09:27:41 -0400

    use jquery@3.4.1 to support bootsrap js

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-// import 'jquery';
-// import 'popper.js';
-// import 'bootstrap';
+import '../node_modules/jquery/dist/jquery.js';
+import '../node_modules/popper.js/dist/popper.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
 
 if (environment.production) {
   enableProdMode();
